7dc95eb3430ecb8a6dd8ad28e96762d6a0934610
Kameshwar Maurya <kameshwarmaurya,ietagra@gmail.com>
2021-05-17 19:27:01 +0530

    my first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let currDate = new Date();
+let cssStyle = {}
+currDate = currDate.getHours();
+let greeting = '';
+if(currDate >=1  && currDate <12){
+  greeting = 'Good Morning';
+  cssStyle.color = 'green';
+}else if(currDate >=12 && currDate <17){
+  greeting = 'Good Afternoon';
+  cssStyle.color = 'orange';
+}else if(currDate >=17 && currDate <22){
+  greeting = 'Good Evening';
+  cssStyle.color = 'blue';
+}else{
+  greeting = 'Good Night';
+  cssStyle.color = 'red';
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Hello Sir, <span style = {cssStyle}>{greeting}</span></h1>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
